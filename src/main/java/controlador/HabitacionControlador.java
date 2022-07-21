aa887e7568d7cf01cb2083e51fb39021c4a4a664
diegoAP20 <105756894+diegoAP20@users.noreply.github.com>
2022-07-20 20:56:38 -0500

    Modifique funcionamiento de habitacion controlador

--- src/main/java/controlador/HabitacionControlador.java
+++ src/main/java/controlador/HabitacionControlador.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.servlet.RequestDispatcher;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,6 +17,9 @@ import modelo.Habitacion;
 
 @WebServlet(description = "administra peticiones para la tabla productos", urlPatterns = {"/HabitacionControlador"})
 public class HabitacionControlador extends HttpServlet {
+
+    HabitacionDAO hdao = new HabitacionDAO();
+    Habitacion h = new Habitacion();
 
     private static final long serialVersionUID = 1L;
 
@@ -32,25 +35,33 @@ public class HabitacionControlador extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String accion = request.getParameter("accion");
+        String id = request.getParameter("id");
         switch (accion) {
             case "RoomDetails":
-                String id = request.getParameter("id");
+               
                 //System.out.println("Editar id: " + id);
-                HabitacionDAO hdao = new HabitacionDAO();
-                Habitacion h = new Habitacion();
+
                 try {
-                    h = hdao.listarId(Integer.parseInt(id));
-                    //System.out.println(h);
-                    request.setAttribute("habitacion", h);
-                    RequestDispatcher requestDispatcher = request.getRequestDispatcher("/vistas/verdetalle.jsp");
-                    requestDispatcher.forward(request, response);
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-                break;
-            case "editar":
-                this.editarHabitacion(request, response);
-                break;
+                h = hdao.listarId(Integer.parseInt(id));
+                //System.out.println(h);
+                request.setAttribute("habitacion", h);
+                RequestDispatcher requestDispatcher = request.getRequestDispatcher("/vistas/verdetalle.jsp");
+                requestDispatcher.forward(request, response);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            break;
+            case "EditRoom":
+                try {
+                h = hdao.obtenerHabitacion(Integer.parseInt(id));
+                System.out.println(h);
+                request.setAttribute("habitacion", h);
+                RequestDispatcher requestDispatcher = request.getRequestDispatcher("/vistas/editarHabitacion.jsp");
+                requestDispatcher.forward(request, response);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            break;
             case "eliminar":
                 this.eliminarHabitacion(request, response);
                 break;
@@ -90,7 +101,7 @@ public class HabitacionControlador extends HttpServlet {
             case "buscar":
                 this.habitacionesFiltradas(request, response);
                 break;
-            case "modificar":
+            case "UpdateRoom":
                 this.modificarHabitacion(request, response);
                 break;
             case "AddRoom":
@@ -102,80 +113,91 @@ public class HabitacionControlador extends HttpServlet {
         }
 
     }
-       private void accionDefault(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException{
-                 
+
+    private void accionDefault(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         response.sendRedirect("index.jsp");
     }
-    
-    private void habitacionesFiltradas(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException{
-        List<Habitacion> habitaciones= new HabitacionDAO().listar();
-        List<Habitacion> listaFiltrada= new ArrayList();
+
+    private void habitacionesFiltradas(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Habitacion> habitaciones = new HabitacionDAO().listar();
+        List<Habitacion> listaFiltrada = new ArrayList();
 
         String habitacionFiltrado = request.getParameter("buscarHabitacion");
-        if(habitacionFiltrado!=null && !habitacionFiltrado.equals(""))
-        {
+        if (habitacionFiltrado != null && !habitacionFiltrado.equals("")) {
             /*for(Habitacion habitacion:habitaciones){
                 if(habitacion.getId_tipohabitacion().toLowerCase().contains(habitacionFiltrado.toLowerCase()))
                 {
                     listaFiltrada.add(habitacion);
                 }
-            } */          
-        
+            } */
+
         }
-        request.setAttribute("habitaciones",listaFiltrada);
-        
+        request.setAttribute("habitaciones", listaFiltrada);
+
         request.getRequestDispatcher("vistas/crudHabitaciones.jsp").forward(request, response);
 
     }
-    
-    private void eliminarHabitacion(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException{
+
+    private void eliminarHabitacion(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int id_habitacion = Integer.parseInt(request.getParameter("id_habitacion"));
-       /*Habitacion habitacion = new Habitacion(id_habitacion);
-       int registrosModificados = new HabitacionDAO().eliminar(habitacion);
-       request.getRequestDispatcher("vistas/crudHabitaciones.jsp").forward(request, response);
-    */
-        }
-    
-    private void editarHabitacion(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException{
-        int id_habitacion = Integer.parseInt(request.getParameter("id_habitacion"));
-      /*  Habitacion habitacion = new HabitacionDAO().encontrar(new Habitacion(id_habitacion));
-        request.setAttribute("habitacion",habitacion);
-        request.getRequestDispatcher("vistas/editarHabitacion.jsp").forward(request, response);
-       */ 
+        Habitacion habitacion = new Habitacion(id_habitacion);
+        int registrosModificados = new HabitacionDAO().eliminar(habitacion);
+        request.getRequestDispatcher("vistas/crudHabitaciones.jsp").forward(request, response);
+
     }
-    
-    private void modificarHabitacion(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException{
+
+    private void editarHabitacion(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        System.out.println("Editar id: " + id);
+        HabitacionDAO habitacionDAO = new HabitacionDAO();
+        Habitacion h = new Habitacion();
+        try {
+            h = habitacionDAO.obtenerHabitacion(id);
+            //System.out.println(h);
+            request.setAttribute("habitacion", h);
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/vistas/editarHabitacion.jsp");
+            requestDispatcher.forward(request, response);
+
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+    private void modificarHabitacion(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         int id_habitacion = Integer.parseInt(request.getParameter("id_habitacion"));
-        String id_tipohabitacion=request.getParameter("id_tipohabitacion");
-        int id_piso=Integer.parseInt(request.getParameter("id_piso"));
-        double precio=Double.parseDouble(request.getParameter("precio"));
-        int imagen=Integer.parseInt(request.getParameter("imagen"));
-        String descripcion=request.getParameter("descripcion");
-        int personas_maximas=Integer.parseInt(request.getParameter("personas_maximas"));
-        String disponibilidad=request.getParameter("disponibilidad");
-        int estrellas = Integer.parseInt("estrellas");
-        /*
-        Habitacion habitacion = new Habitacion(id_habitacion,id_tipohabitacion,id_piso,precio,imagen,descripcion,personas_maximas,disponibilidad,estrellas);
+        int id_tipohabitacion = Integer.parseInt(request.getParameter("id_tipohabitacion"));
+        int id_piso = Integer.parseInt(request.getParameter("id_piso"));
+        double precio = Double.parseDouble(request.getParameter("precio"));
+        String descripcion = request.getParameter("descripcion");
+        int personas_maximas = Integer.parseInt(request.getParameter("personas_maximas"));
+        String disponibilidad = request.getParameter("disponibilidad");
+        int estrellas = Integer.parseInt(request.getParameter("estrellas"));
+        
+        Habitacion habitacion = new Habitacion(id_tipohabitacion, id_piso, precio, 
+                descripcion,personas_maximas, disponibilidad, estrellas,id_habitacion);
 
         int registrosModificados = new HabitacionDAO().actualizar(habitacion);
         request.setAttribute("habitacion",habitacion);
-         response.sendRedirect("vistas/crudHabitaciones.jsp");*/
+        System.out.println("" + habitacion);
+         response.sendRedirect("vistas/crudHabitaciones.jsp");
     }
-    
-    private void agregarHabitacion(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException{
-        int id_tipohabitacion=Integer.parseInt(request.getParameter("id_tipohabitacion"));
-        int id_piso=Integer.parseInt(request.getParameter("id_piso"));//listo
-        double precio=Double.parseDouble(request.getParameter("precio"));//listo
-        String imagen=request.getParameter("imagen");//listo
-        String descripcion=request.getParameter("descripcion");//listo
-        int personas_maximas=Integer.parseInt(request.getParameter("personas_maximas"));//listo
-        String disponibilidad=request.getParameter("disponibilidad");//listo
+
+    private void agregarHabitacion(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id_tipohabitacion = Integer.parseInt(request.getParameter("id_tipohabitacion"));
+        int id_piso = Integer.parseInt(request.getParameter("id_piso"));//listo
+        double precio = Double.parseDouble(request.getParameter("precio"));//listo
+        String imagen = request.getParameter("imagen");//listo
+        String descripcion = request.getParameter("descripcion");//listo
+        int personas_maximas = Integer.parseInt(request.getParameter("personas_maximas"));//listo
+        String disponibilidad = request.getParameter("disponibilidad");//listo
         int estrellas = Integer.parseInt(request.getParameter("estrellas"));//listo
-     
-        Habitacion habitacion = new Habitacion(id_tipohabitacion, id_piso, precio, imagen, 
+
+        Habitacion habitacion = new Habitacion(id_tipohabitacion, id_piso, precio, imagen,
                 descripcion, personas_maximas, disponibilidad, estrellas);
         System.out.println("" + habitacion);
         int cantidadInserto = new HabitacionDAO().insertar(habitacion);
         response.sendRedirect("vistas/crudHabitaciones.jsp");
-    }//que chch es esto pndj, eso no puede hacer .-. ahora tu jsp
+    }
 }
