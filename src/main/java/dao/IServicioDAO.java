6221c6ad80dacfcc99c1a8edd49e46bc8d23fda0
diegoAP20 <105756894+diegoAP20@users.noreply.github.com>
2022-07-20 21:11:01 -0500

    Update IServicioDAO.java

--- src/main/java/dao/IServicioDAO.java
+++ src/main/java/dao/IServicioDAO.java
@@ -12,6 +12,13 @@ public interface IServicioDAO {
     String SELECT_FILTRA_SERVICIO_ID = "SELECT nombre_servicio,precio,horario_inicio,horario_fin,ambiente,estado,personas_maximas FROM servicio where id_servicio=?";
     String CANTIDAD_SERVICIOS = "select count(id_servicio) from servicio";
     
+        public List<Servicio> listar();
+    public Servicio listarId(int id) throws SQLException;
+    public int insertar(Servicio servicio);
+    public int actualizar(Servicio servicio);
+    public int eliminar(Servicio servicio);
+    public Servicio obtenerServicio(int id) throws SQLException;
+    
     List<Servicio> filtrarServicios(String nombreServicio);
     int insertarServicios(Servicio servicio);
     List<String> nombreServicios();
